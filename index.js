8c5a0db9968e10959c2f46fd62f644d64c04a706
Your Name <you@example.com>
2024-01-17 10:48:29 +0100

    dernier commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const apikeys='&appid=fffdb3f7f74de0730b4bc6b194f3e2be&units=metric'
 const ApiUrl="https://api.openweathermap.org/data/2.5/weather?q="
 const searchBox=document.querySelector('.search input');
 const searchBtn=document.querySelector('.search button');
-
+const weatherIcones=document.querySelector('.weather #weatherIcones');
 // lien api : https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=fffdb3f7f74de0730b4bc6b194f3e2be&units=metric
 
 // site https://openweathermap.org
@@ -16,6 +16,32 @@ async function weathers(city) {
   document.querySelector('.temperater').innerHTML=Math.round( data.main.temp)  +'°C';
   document.querySelector('.humidity').innerHTML=data.main.humidity + "%";
   document.querySelector('.wind').innerHTML=data.wind.speed + " km/h";
+
+  //utilisation des icone
+  if(data.weather[0].main == "Clouds"){
+    weatherIcones.src='images/clouds.png';
+  }
+ else if(data.weather[0].main == "Clear"){
+    weatherIcones.src='images/clear.png';  
+ }
+
+
+ else if(data.weather[0].main == "Drizzle"){
+    weatherIcones.src='images/drizzle.png';  
+ }
+
+ else if(data.weather[0].main == "Mist"){
+    weatherIcones.src='images/mist.png';  
+ }
+
+ else if(data.weather[0].main == "Rain"){
+    weatherIcones.src='images/rain.png';  
+ }
+
+ else if(data.weather[0].main == "Snow"){
+    weatherIcones.src='images/snow.png';  
+ }
+
 
 }
 searchBtn.addEventListener('click',()=>{
